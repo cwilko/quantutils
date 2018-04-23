40a59d59b2826280fd49a94582797808d562f819
Chris Wilkinson <Chris.Wilkinson@uk.ibm.com>
2018-04-23 22:53:39 +0100

    Retrieve weights from ml models

--- quantutils/model/ml.py
+++ quantutils/model/ml.py
@@ -96,7 +96,7 @@ class Model():
         return self.NUM_FEATURES
 
     def getWeights(self):
-        return [self.Theta1.eval().tolist(), self.Theta2.eval().tolist()]
+        return np.concatenate([self.Theta1.eval().flatten(), self.Theta2.eval().flatten()]).tolist()
 
     def train(self, train_dict, val_dict, test_dict, threshold, iterations=50, debug=True):
         
