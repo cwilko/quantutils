9044f0636ce6403174c81c0e95c55eef411b276d
cwilko <chris.wilkinson@uk.ibm.com>
2018-08-10 15:05:42 +0100

    Aggregation of predictions

--- quantutils/model/utils.py
+++ quantutils/model/utils.py
@@ -3,26 +3,69 @@ import numpy as np
 import pandas as pd
 import quantutils.dataset.pipeline as ppl
 
-def evaluate(predictions, data_y, threshold, majorityVote=False):
+def evaluate(predictions, data_y, threshold=0):
     
     a = np.argmax(predictions,axis=1)
     b = np.argmax(data_y,axis=1) 
     
-    if (majorityVote):
-        msk = a[1::2] == a[::2]
-        msk = np.dstack([msk,msk]).flatten()
-        a = a[msk]
-        b = b[msk]
-        predictions = predictions[msk]
-    
-    a = a[(predictions > threshold).any(axis=1)]
-    b = b[(predictions > threshold).any(axis=1)]
+    a = a[(predictions >= threshold).any(axis=1)]
+    b = b[(predictions >= threshold).any(axis=1)]
         
-    precision = np.float32(np.sum(a == b) / np.float32(b.shape[0]))
-    recall = np.float32(np.sum(a == b) / np.float32(data_y.shape[0])) # Correct Recall
-    recall = np.float32(b.shape[0]) / data_y.shape[0] # Number of Days traded
-    F_score = (2.0 * precision * recall) / (precision + recall)
-    return precision, recall, F_score
+    num = np.float32(np.sum(a == b))
+    den = np.float32(b.shape[0])
+
+    print("Won : " + str(num))
+    print("Lost : " + str(den - num))
+    print("Total : " + str(den))
+    print("Diff : " + str(num-(den-num)))
+    print("Edge : " + str(100*(num-(den-num))/den) +"%")
+    print("IR : " + str(((num-(den-num))/den)*np.sqrt(den)))
+
+    #recall = np.float32(b.shape[0]) / data_y.shape[0] # Number of Days traded
+    #F_score = (2.0 * precision * recall) / (precision + recall)
+        
+    return num / den # precision
+
+## aggregatePredictions:
+##
+## Options
+#1 np.nanmean(results["test_predictions"], axis=0)
+#2 np.nanmean(results["test_predictions"]) ## Remove conflicting predictions across markets
+#3 (np.sum(np.sign(r-.5), axis=0)/40)+.5 ## Vote rather than average (removes effects of outliers). Voting will ignore the strength of the individual prediction.
+#4 (np.sum(np.sign(r-.5))/80)+.5  ## Vote and Remove conflicting predictions across markets
+#5 as #1 but don't score any entries that are in conflict
+#6 as #3 but don't score any entries that are in conflict
+
+def aggregatePredictions(predictions_list, method='vote_unanimous_all'):
+    a = predictions_list[0]
+    if (method=='mean_all'): ## Remove conflicting predictions across markets
+        for predictions in predictions_list[1:]:
+            a = a.add(predictions)            
+        a = a / len(predictions_list)
+        result = a.mean(axis=1).to_frame()
+    if (method=='vote_majority'): ## Vote rather than average (removes effects of outliers) across all data. Voting will ignore the strength of the individual prediction.
+        a = np.sign(a-.5)
+        for predictions in predictions_list[1:]:
+            a = a.add(np.sign(predictions-.5))            
+        result = ((a.sum(axis=1)/ (predictions_list[0].shape[1] * len(predictions_list) * 2)) +.5).to_frame()
+    if (method=='vote_unanimous_pred'): ## Vote and Remove conflicting predictions across sub-predictions
+        a = np.sign(np.sign(a-.5).sum(axis=1))
+        for predictions in predictions_list[1:]:
+            a = a.add(np.sign(np.sign(predictions-.5).sum(axis=1)))            
+        result = (a/ (len(predictions_list) * 2) +.5).to_frame()
+    if (method=='vote_unanimous_markets'): ## Vote and Remove conflicting predictions across markets
+        a = np.sign(a-.5)
+        for predictions in predictions_list[1:]:
+            a = a.add(np.sign(predictions-.5))            
+        a = ((a.sum(axis=1)/ (predictions_list[0].shape[1] * len(predictions_list) * 2)) +.5)
+        result = a[a!=.5].to_frame()
+    if (method=='vote_unanimous_all'): ## Vote and Remove conflicting predictions across sub-predictions and markets
+        a = np.sign(np.sign(a-.5).sum(axis=1))
+        for predictions in predictions_list[1:]:
+            a = a.add(np.sign(np.sign(predictions-.5).sum(axis=1)))
+        a = (a/((len(predictions_list) * 2)))+.5
+        result = a[a!=.5].to_frame()
+    return result
 
 # TODO : Rename, these are not signals, they are "wins/losses"
 def getSignals(predictions, data_y, threshold):
